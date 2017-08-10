83d06ece46a039cf3895a86da3fb33ecaf6f2230
aidang <aidan.gilbert@thisisnumero.com>
2017-08-10 09:52:06 +0100

    Adds support for transfering environment variables to the elevated process.
     Config;
     elevator.env.names is a string array of environment variables to transfer.
     elevator.env.cb is a call back string function(string name, string value)
     returning the value to be transferred.
     Use this to optionally modify the value of a variable before it's transferred.
    
     For example;
    
     grunt.config('elevator.env.names', ["HOME", "PATH"]);
     grunt.config('elevator.env.cb', function (name, value) {
       if (name == "HOME" && process.env["HOMESHARE"]) {
         return process.env["HOMESHARE"];
       }
     });
    
     This will copy HOME and PATH while substituting the value of HOME
     with the value of HOMESHARE.

--- index.js
+++ index.js
@@ -1,29 +1,91 @@
 var escalatedSpawn = require('@mh-cbon/aghfabsowecwn').spawn;
-var execSync= require("child_process").execSync;
+var execSync = require("child_process").execSync;
 var hooker = require("hooker");
+var path = require('path');
 "use strict";
 
+if (!String.prototype.hashCode) {
+  //A hash code implementation copied from: http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+  String.prototype.hashCode = function () {
+    var result = 0;
+    for (var a = 0; a < this.length; a++) {
+      var character = this.charCodeAt(a);
+      result = ((result << 5) - result) + character;
+      result |= 0;
+    }
+    return result;
+  };
+}
+
+//This file only exists for a moment and is hopefully unique enough.
+//Usually we would use the PID but this changes between normal and elevated states.
+var envFile = path.join(process.env["TEMP"], "path." + process.argv.join().hashCode() + ".tmp");
+
 module.exports = function (grunt) {
-  var elevatedOk= false;
+
+  var save_env = function () {
+    if (grunt.file.exists(envFile)) {
+      grunt.verbose.writeln("Environment has already been saved.");
+      return;
+    }
+    var env = {}
+    //The environment variable names to save.
+    var names = grunt.config.get("elevator.env.names");
+    if (names) {
+      var cb = grunt.config.get("elevator.env.cb");
+      for (var a in names) {
+        var key = names[a];
+        var value = process.env[key];
+        if (cb) {
+          value = cb(key, value) || value;
+        }
+        env[key] = value;
+        grunt.verbose.writeln("Saving environment variable: " + key + " => " + value);
+      }
+    }
+
+    grunt.file.write(envFile, JSON.stringify(env));
+    grunt.verbose.writeln("Saved environment variables to " + envFile);
+  };
+
+  var restore_env = function () {
+    if (!grunt.file.exists(envFile)) {
+      grunt.verbose.writeln("No environment to restore.");
+      return;
+    }
+    var env = JSON.parse(grunt.file.read(envFile));
+    for (var key in env) {
+      var value = env[key];
+      process.env[key] = value;
+      grunt.verbose.writeln("Restored environment variable: " + key + " => " + value);
+    }
+    grunt.file.delete(envFile, { force: true });
+    grunt.verbose.writeln("Restored environment variables from " + envFile);
+  };
+
+  var elevatedOk = false;
+
   hooker.hook(grunt.task, "runTaskFn", {
-    pre: function(context,fn,done,asyncDone) {
+    pre: function (context, fn, done, asyncDone) {
       try {
-        if(!elevatedOk) {
-          execSync( "net session", {"stdio":"ignore"} );
+        if (!elevatedOk) {
+          execSync("net session", { "stdio": "ignore" });
           // If we make it to here carry on as normal.
           grunt.log.ok("Gruntfile is being executed by an elevated user.");
-          elevatedOk= true;
+          restore_env();
+          elevatedOk = true;
         }
       }
-      catch(e) {
-        // Assuming we get an error here because accses is denied.
+      catch (e) {
+        // Assuming we get an error here because access is denied.
         grunt.log.warn("User elevation of Gruntfile is required");
         grunt.task.clearQueue();
+        save_env();
         var opts = {
           bridgeTimeout: 10000,
           stdio: 'pipe'
         };
-        var child= escalatedSpawn(process.argv[0], process.argv.slice(1),opts);
+        var child = escalatedSpawn(process.argv[0], process.argv.slice(1), opts);
         child.stdout.pipe(process.stdout);
         child.stderr.pipe(process.stderr);
         child.on('exit', function (code) {
@@ -31,10 +93,10 @@ module.exports = function (grunt) {
         });
         // if UAC is not validated, or refused, an error is emitted
         child.on('error', function (error) {
-          if (error.code==='ECONNREFUSED') {
+          if (error.code === 'ECONNREFUSED') {
             grunt.fail.fatal('UAC was probably not validated.');
           }
-        });            
+        });
         return hooker.preempt();
       }
     }
